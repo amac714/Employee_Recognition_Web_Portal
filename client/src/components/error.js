f93b9dff84abe2557cdc7045c48c93d8d0c7459a
Alan <amac714@gmail.com>
2019-01-21 12:26:28 -0800

    updated Navbar and added home component

---
+++ client/src/components/error.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ErrorPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Error! Something went wrong</h2>
+      </div>
+    )
+  }
+}
+
+export default ErrorPage;
--- client/src/components/userLogin.js
+++ client/src/components/userLogin.js
@@ -1,26 +1,11 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 class UserLogin extends Component {
-  // Feel free to delete this state, this is just to use "hello ogma" route from api
-  state = {
-    msg: ''
-  }
-
-  //feel free to change or delete
-  componentDidMount(){
-    axios.get('http://localhost:5000/')
-    .then(res => { 
-      console.log(res);
-      this.setState({msg: res.data.data})
-    })
-    .catch(err => console.log(err))
-  }
 
   render() {
     return(
       <div>
-        <h1>{this.state.msg}</h1>
+        <h1>This is the user login page</h1>
       </div>
     );
   }
