4aa77378302f8c181c0e358b43ee64b09f647fda
Phung Ton <phungiaton@gmail.com>
2019-02-01 09:28:49 -0800

    fix built errors

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b54f3a5088264044554a0228d66aa2aa.js"
+  "/precache-manifest.8eeac78c01ebdc319059b05effdf2fee.js"
 );
 
 workbox.clientsClaim();
