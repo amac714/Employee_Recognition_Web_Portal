8cecd5f72435febc7caed0785535050406ba98c7
Phung Ton <phungiaton@gmail.com>
2019-03-04 12:50:43 -0800

    build issues

--- client/build/precache-manifest.4700b58465519009baa83198ad5c4bbc.js
+++ client/build/precache-manifest.4700b58465519009baa83198ad5c4bbc.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "3d987ebe706a6c8e7fe9",
-    "url": "/static/js/main.3d987ebe.chunk.js"
+    "revision": "526a3d5039384e9830ee",
+    "url": "/static/js/main.526a3d50.chunk.js"
   },
   {
     "revision": "53c0cc42c41f27b6074f",
     "url": "/static/js/1.53c0cc42.chunk.js"
   },
   {
-    "revision": "3d987ebe706a6c8e7fe9",
+    "revision": "526a3d5039384e9830ee",
     "url": "/static/css/main.90d94cba.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.5483de55.chunk.css"
   },
   {
-    "revision": "b3846f70d2ecaf209f9a5e56164ceebb",
+    "revision": "e70548a9997599b5d01cc242dd708c1c",
     "url": "/index.html"
   }
 ];
